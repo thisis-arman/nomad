756258ea262bf4a8abf597590f85e8b3cd7b6ad3
thisis-arman <armanhossen1549@gmail.com>
2023-10-18 08:52:31 +0600

    banner

---
+++ app/(MainLayout)/page.js
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import Banner from './Components/HomeComponents/Banner'
+
+export default function Home() {
+  return (
+    <main className="">
+      <Banner/>
+    </main>
+  )
+}
