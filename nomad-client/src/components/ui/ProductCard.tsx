6a010f3e7aba7de32207b919c360777bc3f49faf
Arman Hossen <armanhossen1549@gmail.com>
2024-07-15 23:21:46 +0600

    price filter

--- nomad-client/src/components/ui/ProductCard.tsx
+++ nomad-client/src/components/ui/ProductCard.tsx
@@ -1,34 +1,38 @@
 
-const ProductCard = () => {
+const ProductCard = ({ items }) => {
+    console.log(items);
+
     return (
-        <div>
-            <a href="#" className="group block overflow-hidden">
-                <div className="relative h-[350px] sm:h-[450px]">
-                    <img
-                        src="https://images.unsplash.com/photo-1600185365483-26d7a4cc7519?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1450&q=80"
-                        alt=""
-                        className="absolute inset-0 h-full w-full object-cover opacity-100 group-hover:opacity-0"
-                    />
+        <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-12 ">
+            {
+                items?.map((item,i) => <a href="#" key={i} className="group block overflow-hidden">
+                    <div className="relative h-[200px] sm:h-[300px]">
+                        <img
+                            src={item?.images}
+                            alt=""
+                            className="absolute inset-0 h-full w-full object-contain opacity-100 group-hover:opacity-0"
+                        />
 
-                    <img
-                        src="https://images.unsplash.com/photo-1600185365926-3a2ce3cdb9eb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1450&q=80"
-                        alt=""
-                        className="absolute inset-0 h-full w-full object-cover opacity-0 group-hover:opacity-100"
-                    />
-                </div>
-
-                <div className="relative bg-white pt-3">
-                    <h3 className="text-sm text-gray-700 group-hover:underline group-hover:underline-offset-4">
-                        Limited Edition Sports Trainer
-                    </h3>
-
-                    <div className="mt-1.5 flex items-center justify-between text-gray-900">
-                        <p className="tracking-wide">$189.99</p>
-
-                        <p className="text-xs uppercase tracking-wide">6 Colors</p>
+                        <img
+                            src="https://images.unsplash.com/photo-1600185365926-3a2ce3cdb9eb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1450&q=80"
+                            alt=""
+                            className="absolute inset-0 h-full w-full object-cover opacity-0 group-hover:opacity-100"
+                        />
                     </div>
-                </div>
-            </a>
+
+                    <div className="relative bg-white pt-3">
+                        <h3 className="text-sm text-gray-700 group-hover:underline group-hover:underline-offset-4">
+                            {item.productName}
+                        </h3>
+
+                        <div className="mt-1.5 flex items-center justify-between text-gray-900">
+                            <p className="tracking-wide">$ {item.price}</p>
+
+                            <p className="text-xs uppercase tracking-wide">6 Colors</p>
+                        </div>
+                    </div>
+                </a>)
+            }
         </div>
     );
 };
--- nomad-client/src/pages/Products.tsx
+++ nomad-client/src/pages/Products.tsx
@@ -1,14 +1,46 @@
+import { Button } from "../components/ui/button";
+import { Input } from "../components/ui/input";
+
+import ProductCard from "../components/ui/ProductCard";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "../components/ui/select";
+import { Slider } from "../components/ui/slider";
 import { useGetProductsQuery } from "../redux/features/products/productApi";
 
 
+
 const Products = () => {
-    const { data } = useGetProductsQuery(undefined);
+    const { data, isLoading } = useGetProductsQuery(undefined);
+    if (isLoading) {
+        return <p>Loading ...</p>
+    }
+
+
+    const productPrices = data?.data?.map((a) => a.price)
+    const minimumPriceOfProducts = Math.min(...productPrices);
+    const maximumPriceOfProducts = Math.max(...productPrices);
+
 
     return (
         <div>
 
-            <h1>Show All Products {data.data.length}</h1>
-        </div>
+
+            <div className="my-5 grid md:grid-cols-12 justify-between border gap-4">
+                <div className="h-screen bg-slate-500 md:col-span-3 p-4 ">
+                    <div className="flex justify-between m-4">
+                        <Input className="w-24 " type="number" name="minPrice" defaultValue={minimumPriceOfProducts} />
+                        <Input className="w-24 " type="number" name="maxPrice" defaultValue={maximumPriceOfProducts} />
+                    </div>
+                    <Slider defaultValue={[100]} min={0} max={100} step={1} />
+
+                </div>
+
+                <div className="md:col-span-8 col-span-1">
+                    <ProductCard items={data.data} />
+
+                </div>
+            </div>
+
+        </div >
     );
 };
 
