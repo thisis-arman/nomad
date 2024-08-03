c5d796ca3fc21e01b7308dbb4745c5136861f7c3
Arman Hossen <armanhossen1549@gmail.com>
2024-08-03 12:17:06 +0600

    catchAsync

--- nomad-backend/src/app/utils/sendResponse.ts
+++ nomad-backend/src/app/utils/sendResponse.ts
@@ -1,24 +1,20 @@
-import { Response } from "express"
-
+import { Response } from "express";
 
 type TResponse<T> = {
-    statusCode: number;
-    success: boolean;
-    message: string;
-    data:T
-}
+  statusCode: number;
+  success: boolean;
+  message: string;
+  data: T;
+};
 
-const sendResponse =<T> (res:Response,data:TResponse<T>) => {
- 
-    
-    res.status(data.statusCode).json({
-        success: data.success,
-        message: data.message,
-        data:data.data
-    })
+const sendResponse = <T>(res: Response, data: TResponse<T>) => {
+  res.status(data.statusCode).json({
+    success: data.success,
+    message: data.message,
+    data: data.data,
+  });
 
-    return sendResponse;
-    
-}
+  return sendResponse;
+};
 
-exc\ty,.\
+export default sendResponse;
